61209be25dd73616b0765736deb8999436f25517
ricardopresto <ricardopresto5@gmail.com>
2019-04-03 12:02:00 +0200

    first commit

---
+++ src/Clock.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return <h1>{this.state.date.toLocaleTimeString()}</h1>;
+  }
+}
+export default Clock;
